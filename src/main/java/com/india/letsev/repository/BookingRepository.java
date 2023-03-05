5a96ed7dda45a38aebef2a057f86fd76835dca58
Eswarchandrachedella <eswar.chandra2001@gmail.com>
2023-03-05 19:18:48 +0530

    Booking Journey completed(service,repository).

--- src/main/java/com/india/letsev/repository/BookingRepository.java
+++ src/main/java/com/india/letsev/repository/BookingRepository.java
@@ -1,4 +1,21 @@
 package com.india.letsev.repository;
 
+import com.india.letsev.constants.QueryConstants;
+import com.india.letsev.exception.LetsEVDBException;
+import com.india.letsev.util.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
 public class BookingRepository {
+    public boolean createBookingTable() throws LetsEVDBException {
+        try{
+            Connection connection= ConnectionUtil.getConnection();
+            Statement statement=connection.createStatement();
+            return statement.execute(QueryConstants.CREATE_BOOKING_QUERY);
+
+        }catch(Exception e){
+            throw new LetsEVDBException("Exception occured while creating the Booking Table",e);
+        }
+    }
 }

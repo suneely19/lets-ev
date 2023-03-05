030d161256a4b28d53e5eb6fb558e4b83b0d6934
naveenkodipalli <kodipallinaveen12@gmail.com>
2023-03-05 10:54:33 +0530

    commit admin repository comleted xisoqkw;l

--- src/main/java/com/india/letsev/servlet/AdminRegisterServlet.java
+++ src/main/java/com/india/letsev/servlet/AdminRegisterServlet.java
@@ -11,5 +11,6 @@ public class AdminRegisterServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+
     }
 }

c37ff4a6ee165e94f0e32923701437382cd14df2
suneely19 <tech.suneel19@gmail.com>
2023-03-06 08:03:37 +0530

    Car registration journey Repository implemented

--- src/main/java/com/india/letsev/constants/QueryConstants.java
+++ src/main/java/com/india/letsev/constants/QueryConstants.java
@@ -7,7 +7,7 @@ public class QueryConstants {
     public static final String INSERT_ADMIN_QUERY="insert into admin(id,name,mailid,address,mobile,experiance,salary) values(?,?,?,?,?,?,?)";
     public static final String UPDATE_ADMIN_QUERY="update admin set name=?, mailid=?,address=?,mobile=?,experiance=?,salary=? where id=?";
     public static final String DELETE_ADMIN_QUERY="delete admin where id=?";
-
+    public static final String INSERT_CAR_QUERY="insert into car(id,brand,model,colour,features,seating_capacity,fuel_type,verified,milage,chasis_no,registration_no,engine_no,verified_admin_id) values(?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
 
 }
--- src/main/java/com/india/letsev/dto/CarDTO.java
+++ src/main/java/com/india/letsev/dto/CarDTO.java
@@ -1,4 +1,140 @@
 package com.india.letsev.dto;
 
 public class CarDTO {
+   private int id;
+   private String brand;
+   private String model;
+   private String colour;
+   private String features;
+   private int seatingcapacity;
+   private String fuelType;
+   private String veified;
+   private double milage;
+   private String chasisno;
+   private String registrationno;
+   private String engineno;
+   private int verfiedAdminId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(String features) {
+        this.features = features;
+    }
+
+    public int getSeatingcapacity() {
+        return seatingcapacity;
+    }
+
+    public void setSeatingcapacity(int seatingcapacity) {
+        this.seatingcapacity = seatingcapacity;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public String getVeified() {
+        return veified;
+    }
+
+    public void setVeified(String veified) {
+        this.veified = veified;
+    }
+
+    public double getMilage() {
+        return milage;
+    }
+
+    public void setMilage(double milage) {
+        this.milage = milage;
+    }
+
+    public String getChasisno() {
+        return chasisno;
+    }
+
+    public void setChasisno(String chasisno) {
+        this.chasisno = chasisno;
+    }
+
+    public String getRegistrationno() {
+        return registrationno;
+    }
+
+    public void setRegistrationno(String registrationno) {
+        this.registrationno = registrationno;
+    }
+
+    public String getEngineno() {
+        return engineno;
+    }
+
+    public void setEngineno(String engineno) {
+        this.engineno = engineno;
+    }
+
+    public int getVerfiedAdminId() {
+        return verfiedAdminId;
+    }
+
+    public void setVerfiedAdminId(int verfiedAdminId) {
+        this.verfiedAdminId = verfiedAdminId;
+    }
+
+    @Override
+    public String toString() {
+        return "CarDTO{" +
+                "id=" + id +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", colour='" + colour + '\'' +
+                ", features='" + features + '\'' +
+                ", seatingcapacity=" + seatingcapacity +
+                ", fuelType='" + fuelType + '\'' +
+                ", veified='" + veified + '\'' +
+                ", milage=" + milage +
+                ", chasisno='" + chasisno + '\'' +
+                ", registrationno='" + registrationno + '\'' +
+                ", engineno='" + engineno + '\'' +
+                ", verfiedAdminId=" + verfiedAdminId +
+                '}';
+    }
 }

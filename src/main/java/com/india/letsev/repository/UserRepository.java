b983329afbd83d227e31dc53b99d7e51b664ee76
annamayya23 <kesannagariannamayya@gmail.com>
2023-03-06 15:09:14 +0530

    user journey2 userDTO and userrepository implemented

--- src/main/java/com/india/letsev/repository/UserRepository.java
+++ src/main/java/com/india/letsev/repository/UserRepository.java
@@ -1,20 +1,40 @@
 package com.india.letsev.repository;
 
 import com.india.letsev.constants.QueryConstants;
+import com.india.letsev.dto.UserDTO;
 import com.india.letsev.exception.LetsEVDBException;
 import com.india.letsev.util.ConnectionUtil;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 
 public class UserRepository {
-    public boolean createUserTable() throws LetsEVDBException {
+    public int registerUser(UserDTO userDTO) throws LetsEVDBException {
+        Connection connection=null;
+        PreparedStatement preparedStatement=null;
         try{
-            Connection connection= ConnectionUtil.getConnection();
-            Statement statement=connection.createStatement();
-            return statement.execute(QueryConstants.CREATE_USER_QUERY);
+            connection= ConnectionUtil.getConnection();
+            preparedStatement =connection.prepareStatement(QueryConstants.INSERT_USER_QUERY);
+
+            preparedStatement.setInt(1,userDTO.getId());
+            preparedStatement.setString(2, userDTO.getName());
+            preparedStatement.setString(3, userDTO.getMailid());
+            preparedStatement.setString(3, userDTO.getAddress());
+            preparedStatement.setString(4, userDTO.getMobileno());
+            preparedStatement.setString(5, userDTO.getMobileno());
+            preparedStatement.setString(6, userDTO.getLicence());
+
+            return preparedStatement.executeUpdate();
         }catch (Exception e){
-            throw new LetsEVDBException("Exception occured while creating the User table", e);
+            throw new LetsEVDBException("Exception occured while registering the User", e);
+        }finally {
+            try {
+                preparedStatement.close();
+                connection.close();
+            }catch (Exception e){
+                throw new LetsEVDBException("Exception occured while closing the connection");
+            }
         }
     }
 }

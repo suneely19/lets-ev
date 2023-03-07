7775be71ccefe7fa667ad8ca5dcc8dc438563b44
Debasis <debasiscarlo@gmail.com>
2023-03-07 08:18:13 +0530

    Create table booking query added in Booking repository

--- src/main/java/com/india/letsev/repository/BookingRepository.java
+++ src/main/java/com/india/letsev/repository/BookingRepository.java
@@ -13,37 +13,47 @@ import java.sql.PreparedStatement;
 import java.sql.Statement;
 
 public class BookingRepository {
-    public int insertBooking(BookingDTO bookingDTO) throws LetsEVDBException {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        try {
-            connection = ConnectionUtil.getConnection();
-            preparedStatement = connection.prepareStatement(QueryConstants.INSERT_BOOKING_QUERY);
 
-            preparedStatement.setInt(1, bookingDTO.getId());
-            preparedStatement.setString(2, bookingDTO.getUser_id());
-            preparedStatement.setInt(3, bookingDTO.getCar_id());
-            preparedStatement.setString(4, bookingDTO.getBoarding());
-            preparedStatement.setString(5, bookingDTO.getDestination());
-            preparedStatement.setInt(6, bookingDTO.getDuration());
-            preparedStatement.setDouble(7, bookingDTO.getAmount());
+	public boolean createBookingTable() throws LetsEVDBException {
+		try {
+			Connection connection = ConnectionUtil.getConnection();
+			Statement statement = connection.createStatement();
+			return statement.execute(QueryConstants.CREATE_BOOKING_QUERY);
 
-            return preparedStatement.executeUpdate();
+		} catch (Exception e) {
+			throw new LetsEVDBException("Exception occured while creating the Booking table", e);
 
+		}
+	}
 
-        } catch (Exception e) {
-            throw new LetsEVDBException("Exception occured while Booking the Car", e);
-        } finally {
-            try {
-                preparedStatement.close();
-                connection.close();
-            } catch (Exception e) {
-                throw new LetsEVDBException("Exception occured while closing the connection", e);
-            }
-        }
+	public int insertBooking(BookingDTO bookingDTO) throws LetsEVDBException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		try {
+			connection = ConnectionUtil.getConnection();
+			preparedStatement = connection.prepareStatement(QueryConstants.INSERT_BOOKING_QUERY);
 
-    }
+			preparedStatement.setInt(1, bookingDTO.getId());
+			preparedStatement.setString(2, bookingDTO.getUser_id());
+			preparedStatement.setInt(3, bookingDTO.getCar_id());
+			preparedStatement.setString(4, bookingDTO.getBoarding());
+			preparedStatement.setString(5, bookingDTO.getDestination());
+			preparedStatement.setInt(6, bookingDTO.getDuration());
+			preparedStatement.setDouble(7, bookingDTO.getAmount());
+
+			return preparedStatement.executeUpdate();
+
+		} catch (Exception e) {
+			throw new LetsEVDBException("Exception occured while Booking the Car", e);
+		} finally {
+			try {
+				preparedStatement.close();
+				connection.close();
+			} catch (Exception e) {
+				throw new LetsEVDBException("Exception occured while closing the connection", e);
+			}
+		}
+
+	}
 
 }
-
-

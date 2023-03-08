aca25a54494e225611aa40057bdfe58562a9894d
VasuKotakonda <vasukotakonda2001@gmail.com>
2023-03-08 07:20:15 +0530

    its showing an error

--- src/main/java/com/india/letsev/service/CarService.java
+++ src/main/java/com/india/letsev/service/CarService.java
@@ -1,4 +1,21 @@
 package com.india.letsev.service;
 
+import com.india.letsev.exception.LetsEVDBException;
+import com.india.letsev.exception.LetsEVGeneralException;
+import com.india.letsev.repository.AdminRepository;
+import com.india.letsev.repository.CarRepository;
+
 public class CarService {
+    CarRepository repository;
+    public CarService(){
+        repository=new CarRepository();
+    }
+
+
+    public boolean createAdminTable()throws LetsEVGeneralException {
+        try {
+            return repository.createCarTable();
+        }catch (LetsEVDBException e){
+            throw new LetsEVGeneralException("Exception occured while creating Car table",e);
+        }
 }
